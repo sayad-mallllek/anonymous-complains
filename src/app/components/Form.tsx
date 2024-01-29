70efc952a89912e432112436009f25545977997e
sayad-mallllek <bob@khalilelzein.com>
2024-01-29 22:13:53 +0200

    preline integration and names

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { FormEventHandler } from "react";
+import Select from "./Select";
+import TextArea from "./Textarea";
 
 const Form = () => {
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
@@ -17,15 +19,14 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div className="flex flex-col gap-6">
-        <textarea
-          className="break-words resize-none max-h-96 h-[90vh] max-w-[700px] w-[90vw]"
-          placeholder="Enter something funny."
-          id="text"
-          name="text"
-          rows={4}
-          required
-        />
-        <button type="submit">Send</button>
+        <Select name="person" />
+        <TextArea name="comment" />
+        <button
+          type="button"
+          className="py-3 px-4 max-w-24 mx-auto inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        >
+          Complain
+        </button>
       </div>
     </form>
   );
---
+++ src/app/components/Textarea.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  name: string;
+}
+
+const TextArea = ({ name }: Props) => {
+  return (
+    <div>
+      <label className="block text-sm font-medium mb-2 dark:text-white">
+        Comment
+      </label>
+      <textarea
+        id="textarea-label"
+        className="py-3 px-4 block w-full   rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:text-gray-400 dark:focus:ring-gray-600"
+        rows={3}
+        placeholder="Write your complaint here..."
+      ></textarea>
+    </div>
+  );
+};
+
+export default TextArea;
